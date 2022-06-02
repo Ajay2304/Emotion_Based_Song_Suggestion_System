79cb68996079af48dac91adb46c3d81571d9d0a8
ajay <rajay230401@gmail.com>
2022-06-02 20:46:46 +0530

    Emotion Based Song Suggestion System

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from pydoc import render_doc
 from django.shortcuts import render
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, Response
 import pymysql
 import pandas as pd
 import json
@@ -10,6 +10,7 @@ import statsmodels.api as sm
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sympy import N
+from deepface import DeepFace
 
 from csv import writer
 
@@ -64,9 +65,14 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator , img_to_arr
 from tensorflow.keras.applications.mobilenet import MobileNet, preprocess_input 
 from tensorflow.keras.losses import categorical_crossentropy
 
-from IPython.display import display, Javascript
-# from google.colab.output import eval_js
-from base64 import b64decode
+
+import spotipy
+
+import cv2
+import datetime, time
+import os, sys
+import numpy as np
+from threading import Thread
 
 app = Flask(__name__, static_url_path='/home/ajay/Desktop/FYP/static')
 def sql_connector():
@@ -144,6 +150,15 @@ def signup():
     return render_template('signup.html')
 @app.route('/refresh', methods=['GET','POST'])
 def refresh():
+
+    CLIENT_ID = "87497b1a09094aa29244bcd9b4c3e843"
+    CLIENT_SECRET = "3a270da264f54cd19fe0c681d8232031"
+
+    #Authentication - without user
+    client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+    sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
+
+
     with open("/home/ajay/Desktop/FYP/static/song.json", "w+") as outfile:
                 json.dump("", outfile, indent = 4)
     data = pd.read_csv("Dataset.csv")
@@ -194,14 +209,7 @@ def refresh():
     ax.yaxis.set_ticklabels(labels)
 
     print("Accuracy Score",accuracy_score(y_test,dtree_predictions))
-    CLIENT_ID = "87497b1a09094aa29244bcd9b4c3e843"
-    CLIENT_SECRET = "3a270da264f54cd19fe0c681d8232031"
-
-    #Authentication - without user
-    client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
-    sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
-
-
+    
     # importing necessary libraries
 
     # loading the iris dataset
@@ -603,18 +611,41 @@ def refresh():
 @app.route('/mood', methods=['GET','POST'])
 def userinputmap():
 
-    
 
+    with open("/home/ajay/Desktop/FYP/static/song.json", "w+") as outfile:
+                json.dump("", outfile, indent = 4)
+    model=load_model("imagerec.h5")
+    op={0: 'ANGRY', 3: 'HAPPY', 2: 'CALM', 1: 'SAD'}
+    path = "image.png"
+    
+    img = load_img(path, target_size=(224,224) )
+    
+    emo = " "
+    i = img_to_array(img)/255
+    input_arr = np.array([i])
+    input_arr.shape
+
+    pred = np.argmax(model.predict(input_arr))
+    if(pred == 0):
+        emo = "ANGRY"
+    elif(pred == 3):
+        emo = "HAPPY"
+    elif(pred == 2):
+        emo = "CALM"
+    elif(pred ==1):
+        emo = "SAD"
+    print(f" the person is {emo}")
     songs_json = {"song_id_image":[]}
     dataset = pd.read_csv('song_id.csv',encoding="utf-8")
     dataframe = dataset.iloc[:,:]
     emotion = ""
+    
     print(dataframe)
     id = {}
     
     if request.method == 'POST':
 
-        userinput = request.form.get('usermood')
+        userinput = emo
         
         if userinput == 'CALM':
             
@@ -723,15 +754,119 @@ def userinputmap():
                         id = {}
             with open("/home/ajay/Desktop/FYP/static/song.json", "w+") as outfile:
                 json.dump(songs_json, outfile, indent = 4)
-        emotion+=userinput
+        emotion+=op[pred]
     
     return render_template('userinput.html', e = emotion)
     
+
+global capture,rec_frame, grey, switch, neg, face, rec, out 
+capture=0
+grey=0
+neg=0
+face=0
+switch=1
+rec=0
+
+#make shots directory to save pics
+# try:
+#     os.mkdir('./shots')
+# except OSError as error:
+#     pass
+
+#Load pretrained face detection model    
+net = cv2.dnn.readNetFromCaffe('./saved_model/deploy.prototxt.txt', './saved_model/res10_300x300_ssd_iter_140000.caffemodel')
+
+#instatiate flask app  
+# app = Flask(__name__, template_folder='./templates')
+
+
+camera = cv2.VideoCapture(0)
+
+def record(out):
+    global rec_frame
+    while(rec):
+        time.sleep(0.05)
+        out.write(rec_frame)
+
+
+def detect_face(frame):
+    global net
+    (h, w) = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,
+        (300, 300), (104.0, 177.0, 123.0))   
+    net.setInput(blob)
+    detections = net.forward()
+    confidence = detections[0, 0, 0, 2]
+
+    if confidence < 0.5:            
+            return frame           
+
+    box = detections[0, 0, 0, 3:7] * np.array([w, h, w, h])
+    (startX, startY, endX, endY) = box.astype("int")
+    try:
+        frame=frame[startY:endY, startX:endX]
+        (h, w) = frame.shape[:2]
+        r = 480 / float(h)
+        dim = ( int(w * r), 480)
+        frame=cv2.resize(frame,dim)
+    except Exception as e:
+        pass
+    return frame
+ 
+
+def gen_frames():  # generate frame by frame from camera
+    global out, capture,rec_frame
+    while True:
+        success, frame = camera.read() 
+        if success:
+            if(face):                
+                frame= detect_face(frame)
+            if(capture):
+                capture=0
+                now = datetime.datetime.now()
+                # p = os.path.sep.join("shot_{}.png".format(str(now).replace(":",'')))
+                cv2.imwrite("image.png", frame)
+            
+            if(rec):
+                rec_frame=frame
+                frame= cv2.putText(cv2.flip(frame,1),"Recording...", (0,25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255),4)
+                frame=cv2.flip(frame,1)
+            
+                
+            try:
+                ret, buffer = cv2.imencode('.jpg', cv2.flip(frame,1))
+                frame = buffer.tobytes()
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            except Exception as e:
+                pass
+                
+        else:
+            pass
+
+# @app.route('/camera')
+# def index():
+#     return render_template('userinput.html')
         
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/requests',methods=['POST','GET'])
+def tasks():
+    global switch,camera
+    if request.method == 'POST':
+        if request.form.get('click') == 'CAPTURE':
+            global capture
+            capture=1                
+                 
+    elif request.method=='GET':
+        return render_template('userinput.html')
+    return render_template('userinput.html')
 
 
-
-# main driver function
 if __name__ == '__main__':
-  
     app.run()
+    
+camera.release()
+cv2.destroyAllWindows()     
